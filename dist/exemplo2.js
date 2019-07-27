1ccab357dfce6539d6d95ba05db95ed6da661a06
Vinicius <viniciusjb2013@hotmail.com>
2019-07-26 22:19:55 -0300

    Exemplo 2 - TS

---
+++ dist/exemplo2.js
@@ -0,0 +1,5 @@
+var quantidade = 5;
+quantidade = "5";
+console.log("Quantidade atual é " + quantidade);
+quantidade = quantidade + 1;
+console.log("Quantidade atualizada é " + quantidade);
---
+++ exemplo2.ts
@@ -0,0 +1,6 @@
+let quantidade: number = 5
+quantidade = "5"
+console.log("Quantidade atual é " + quantidade)
+
+quantidade = quantidade + 1
+console.log("Quantidade atualizada é " + quantidade)
